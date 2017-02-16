d140ecafe31d4af478b0c6061f53b962e368a706
Gabriel Rubens <grsabreu@gmail.com>
2017-02-16 16:34:54 -0300

    First commit :ok_hand:

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import debounce from 'lodash.debounce'
+
+export default class ShowOnScrollTop extends React.Component {
+  static state = {
+    isScrollingToTop: true,
+    lastScrollPosition: 0,
+  }
+
+  scrollHandler = debounce(event => {
+    const scrollPos = window.scrollY
+
+    this.setState({
+      isScrollingToTop: scrollPos < this.state.lastScrollPosition || scrollPos === 0,
+      lastScrollPosition: scrollPos,
+    })
+  }, 200)
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.scrollHandler)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.scrollHandler)
+  }
+
+  render(){
+    return (
+      <div
+        style={{
+          transform: this.state.isScrollingToTop ? 'translateY(0)' : 'translateY(-1000px)',
+          transition: '0.8s ease-out transform',
+        }}
+      >
+        {this.props.children}
+      </div>
+    )
+  }
+}
